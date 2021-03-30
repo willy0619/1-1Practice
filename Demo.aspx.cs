b7b3d19dd931031343128c4d06f7174f1da25b96
willy0619 <74764061+willy0619@users.noreply.github.com>
2021-03-30 16:17:19 +0800

    Add files via upload

---
+++ Demo.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1_1Practice {
+    public partial class Demo : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Response.Write("Hi222");
+            Response.Write("Demo can run.");
+        }
+    }
+}
